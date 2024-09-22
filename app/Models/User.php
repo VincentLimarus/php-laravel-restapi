20d2bdf907e99045b5ab7805602dde7228a57818
Vincent Oliver Limarus <vincentlimarus27@gmail.com>
2024-09-22 22:50:06 +0700

    user models

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,32 +12,21 @@ class User extends Authenticatable
 {
     use HasFactory, Notifiable, HasApiTokens;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
+    protected $table = 'users';
+    
     protected $fillable = [
         'name',
         'email',
-        'password',
+        'password', 
+        'phone_number',
+        'address',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
